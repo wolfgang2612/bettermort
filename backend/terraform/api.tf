89de4f27e7e4a3e52962fbb541a0460c64a9b80d
Ashutosh Mishra <ashutoshepsilon@gmail.com>
2023-12-26 17:10:24 +0900

    Add websocket integrations, stages and d eployment

--- backend/terraform/api.tf
+++ backend/terraform/api.tf
@@ -4,6 +4,11 @@ resource "aws_apigatewayv2_api" "websocket_api" {
   route_selection_expression = "$request.body.action"
 }
 
+resource "aws_apigatewayv2_stage" "api_stage" {
+  api_id = aws_apigatewayv2_api.websocket_api.id
+  name   = "bettermort-stage"
+}
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -43,4 +48,43 @@ resource "aws_lambda_function" "websocket_functions" {
   runtime          = "python3.8"
 }
 
+resource "aws_apigatewayv2_integration" "integration" {
+  for_each         = { for idx, value in var.websocket_routes : idx => value }
+  api_id           = aws_apigatewayv2_api.websocket_api.id
+  integration_type = "AWS_PROXY"
 
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  integration_method        = "POST"
+  integration_uri           = aws_lambda_function.websocket_functions[each.key].invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "routes" {
+  for_each  = { for idx, value in var.websocket_routes : idx => value }
+  api_id    = aws_apigatewayv2_api.websocket_api.id
+  route_key = each.value.route_key
+  target    = "integrations/${aws_apigatewayv2_integration.integration[each.key].id}"
+}
+
+resource "aws_apigatewayv2_route_response" "route_responses" {
+  for_each           = { for idx, value in var.websocket_routes : idx => value }
+  api_id             = aws_apigatewayv2_api.websocket_api.id
+  route_id           = aws_apigatewayv2_route.routes[each.key].id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_deployment" "example" {
+  api_id      = aws_apigatewayv2_api.websocket_api.id
+  description = "Bettermort deployment"
+  depends_on  = [aws_apigatewayv2_route.routes, aws_apigatewayv2_integration.integration, aws_apigatewayv2_route_response.route_responses]
+
+  triggers = {
+    redeployment = sha1(join(",", tolist([
+      jsonencode(aws_apigatewayv2_route.routes),
+      jsonencode(aws_apigatewayv2_integration.integration),
+      jsonencode(aws_apigatewayv2_route_response.route_responses),
+    ])))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
