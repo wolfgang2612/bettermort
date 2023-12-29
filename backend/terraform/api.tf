633be18fb9377555bb1f28109445c09ac31d891f
Ashutosh Mishra <ashutoshepsilon@gmail.com>
2023-12-29 21:03:13 +0900

    Change lambda role name

--- backend/terraform/api.tf
+++ backend/terraform/api.tf
@@ -27,14 +27,14 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+resource "aws_iam_role" "bettermort_lambda_role" {
+  name               = "bettermort_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 
 resource "aws_iam_role_policy_attachment" "lambda_execution_policy" {
-  role       = aws_iam_role.iam_for_lambda.name
+  role       = aws_iam_role.bettermort_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -52,7 +52,7 @@ resource "aws_lambda_function" "websocket_functions" {
   filename      = "${path.module}/lambda_functions/${each.value.function_name}/${each.value.function_name}.zip"
   function_name = each.value.function_name
 
-  role    = aws_iam_role.iam_for_lambda.arn
+  role    = aws_iam_role.bettermort_lambda_role.arn
   handler = "lambda_function.lambda_handler"
 
   source_code_hash = data.archive_file.lambda[each.key].output_base64sha256
