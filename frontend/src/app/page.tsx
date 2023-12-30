d3253319297f3fbc587e700b6e7d035a136038e7
Ashutosh Mishra <ashutoshepsilon@gmail.com>
2023-12-30 14:46:16 +0900

    Test change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import Paper from "@mui/material/Paper";
 export default function Home() {
   return (
     <Paper elevation={3}>
-      <Typography>test change passu racist</Typography>
+      <Typography>checking</Typography>
     </Paper>
   );
 }
