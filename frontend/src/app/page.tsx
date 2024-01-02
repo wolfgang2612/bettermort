c4cca2f9ac78ae46de04b8e5c1beda21b7f88dcf
Ashutosh Mishra <ashutoshepsilon@gmail.com>
2024-01-02 18:29:41 +0900

    Import changes - test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   return (
